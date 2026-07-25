2c8c2439caa658f61b7f11b014c5048d27a25458
somu0571 <somcha2006@gmail.com>
2026-07-25 14:51:17 +0530

    Initial Commit

---
+++ Patterns/charactersPattern.java
@@ -0,0 +1,15 @@
+
+public class charactersPattern {
+    public static void main(String args[]) {
+        int n = 10;
+        char ch = 'A';
+
+        for(int line=1; line <= n; line++) {
+            for(int chars=1; chars<=line; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
