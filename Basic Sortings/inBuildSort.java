9026151c34607e5fb100cab0205d811d36fbecdd
somu0571 <somcha2006@gmail.com>
2026-08-11 22:28:03 +0530

    Initial Commit

---
+++ Basic Sortings/inBuildSort.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class inBuildSort {
+    public static void main(String args[]) {
+        Integer arr[] = { 6, 18, 20, 9, 12 };
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
